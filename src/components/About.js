d530ec35aca1ec5e87cd1f8fba086231981ea25a
Michael <michaelalfaro93@gmail.com>
2023-05-03 21:04:29 -0700

    adds contact.js and helpers.js for contact form functionality

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p className="content is-medium">Technical Writer and Something-of-a Full Stack Developer</p>
+      <hr />
+      <img className="my-pic" src={process.env.PUBLIC_URL + '/img/portfolioicon.jpg'} alt="Michael Alfaro"/>
+      <p className="content is-italic mt-4">
+       I'm Michael, a guy trying to further my knowledge in software developing and tech in general.
+      </p>
+      <p className="content">
+        Learning HTML, CSS, Javascript and it's many variations mainly including react.
+      </p>
+    </div>
+  );
+}
+
+export default About;
